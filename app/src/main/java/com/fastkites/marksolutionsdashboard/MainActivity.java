fd90016940969c690c39f6516696a468079050a4
ismailsalih <ismailsalih721@gmail.com>
2023-12-10 08:46:27 +0530

    Commit

--- app/src/main/java/com/fastkites/marksolutionsdashboard/MainActivity.java
+++ app/src/main/java/com/fastkites/marksolutionsdashboard/MainActivity.java
@@ -1,15 +1,29 @@
 package com.fastkites.marksolutionsdashboard;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton settingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingsBtn = findViewById(R.id.settingsBtn);
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Settings.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/fastkites/marksolutionsdashboard/Settings.java
+++ app/src/main/java/com/fastkites/marksolutionsdashboard/Settings.java
@@ -1,14 +1,32 @@
 package com.fastkites.marksolutionsdashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
+
+    CardView schedule, password, account, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        schedule = findViewById(R.id.schedule);
+        password = findViewById(R.id.password);
+        account = findViewById(R.id.account);
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
     }
 }
